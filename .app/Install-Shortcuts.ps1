586a83c7fae2c2daeaf5742337f217ef75344a6c
tghcgu <76006000+tghcgu@users.noreply.github.com>
2026-06-09 01:13:23 +0900

    Simplify generated shortcuts

--- .app/Install-Shortcuts.ps1
+++ .app/Install-Shortcuts.ps1
@@ -12,11 +12,9 @@ $baseName = -join ([char[]]@(
     0x30ad, 0x30e3, 0x30d7, 0x30b7, 0x30e7, 0x30f3,
     0x6587, 0x5b57, 0x8d77, 0x3053, 0x3057
 ))
-$fileOnlyMode = -join ([char[]]@(0x88cf, 0x4fdd, 0x5b58))
 $stopMode = -join ([char[]]@(0x505c, 0x6b62))
 
 $notepadName = "$baseName.lnk"
-$fileOnlyName = "$baseName $fileOnlyMode.lnk"
 $stopName = "$baseName$stopMode.lnk"
 
 $transcripts = Join-Path $workspace "transcripts"
@@ -66,13 +64,11 @@ function New-AppShortcut {
     $shortcut.Save()
 }
 
-$fileOnlyArgs = "-WindowStyle Hidden -STA -NoProfile -ExecutionPolicy Bypass -File `"$startScript`" -NoPasteToNotepad"
 $notepadArgs = "-WindowStyle Hidden -STA -NoProfile -ExecutionPolicy Bypass -File `"$startScript`""
 $stopArgs = "-WindowStyle Hidden -NoProfile -ExecutionPolicy Bypass -File `"$stopScript`""
 
 foreach ($dir in $shortcutDirs) {
     New-AppShortcut -Directory $dir -Name $notepadName -Arguments $notepadArgs -IconLocation "$notepadIcon,0" -Description "Microsoft Live Captions to Notepad"
-    New-AppShortcut -Directory $dir -Name $fileOnlyName -Arguments $fileOnlyArgs -IconLocation "$notepadIcon,0" -Description "Microsoft Live Captions to text file"
     New-AppShortcut -Directory $dir -Name $stopName -Arguments $stopArgs -IconLocation "$stopIcon,109" -Description "Stop Live Captions transcription and open latest file"
 }
 
